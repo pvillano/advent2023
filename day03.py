d36f4cb5591a5d167ed2add4294b55ee8e414204
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2023-12-03 00:37:29 -0600

    day 3 cleanup

--- day03.py
+++ day03.py
@@ -17,65 +17,63 @@ test = """467..114..
 """
 
 
-def parse(raw: str):
+def parse1(raw: str):
     lines = raw.split("\n")
     line: str
-    for i, line in enumerate(lines):
-        j = 0
-        while j < len(line):
-            ch = line[j]
+    for line_no, line in enumerate(lines):
+        num_start = 0
+        while num_start < len(line):
+            ch = line[num_start]
             if ch.isnumeric():
-                k = j
-                while k < len(line) and line[k].isnumeric():
-                    k += 1
-                # k points past end line or is not numeric
-                num = line[j:k]
+                num_end = num_start
+                while num_end < len(line) and line[num_end].isnumeric():
+                    num_end += 1
+                num = line[num_start:num_end]
                 assert num.isnumeric()
-                ispartnum = False
-                for li in range(max(i - 1, 0), min(i + 2, len(lines))):
-                    for lj in range(max(j - 1, 0), min(k + 1, len(line))):
-                        if lines[li][lj] not in "0123456789.":
-                            ispartnum = True
-                if ispartnum:
+                is_part = False
+                for symb_row in range(max(line_no - 1, 0), min(line_no + 2, len(lines))):
+                    for symb_col in range(max(num_start - 1, 0), min(num_end + 1, len(line))):
+                        if lines[symb_row][symb_col] not in "0123456789.":
+                            is_part = True
+                if is_part:
                     yield int(num)
-                j = k
-            j += 1
+                num_start = num_end
+            num_start += 1
 
 
 def part1(raw: str):
-    return sum(parse(raw))
+    return sum(parse1(raw))
 
 
 def parse2(raw: str):
     lines = raw.split("\n")
     gears = defaultdict(list)
     line: str
-    for i, line in enumerate(lines):
-        j = 0
-        while j < len(line):
-            ch = line[j]
+    for line_no, line in enumerate(lines):
+        num_start = 0  # inclusive
+        while num_start < len(line):
+            ch = line[num_start]
             if ch.isnumeric():
-                k = j
-                while k < len(line) and line[k].isnumeric():
-                    k += 1
-                # k points past end line or is not numeric
-                num = line[j:k]
+                num_end = num_start  # exclusive
+                while num_end < len(line) and line[num_end].isnumeric():
+                    num_end += 1
+                num = line[num_start:num_end]
                 assert num.isnumeric()
-                for li in range(max(i - 1, 0), min(i + 2, len(lines))):
-                    for lj in range(max(j - 1, 0), min(k + 1, len(line))):
-                        if lines[li][lj] == "*":
-                            gears[(li, lj)].append(int(num))
-                j = k
-            j += 1
+                for gear_row in range(max(line_no - 1, 0), min(line_no + 2, len(lines))):
+                    for gear_col in range(max(num_start - 1, 0), min(num_end + 1, len(line))):
+                        if lines[gear_row][gear_col] == "*":
+                            gears[(gear_row, gear_col)].append(int(num))
+                num_start = num_end
+            num_start += 1
     return gears
 
 
 def part2(raw: str):
-    s = 0
+    sum_gears = 0
     for v in parse2(raw).values():
-        if len(v) > 1:
-            s += reduce(operator.mul, v, 1)
-    return s
+        if len(v) == 2:
+            sum_gears += reduce(operator.mul, v, 1)
+    return sum_gears
 
 
 def main():
