ffe57c6c7965ac3a684183325902bd69f55ccc12
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2023-12-07 00:08:41 -0600

    day 7 cleanup

--- day07.py
+++ day07.py
@@ -18,38 +18,34 @@ def parse(raw: str):
         yield cards, int(bet)
 
 
-def key(handbet: str):
-    hand, bet = handbet
+def key(hand_and_bet: tuple[str, int]):
+    hand, bet = hand_and_bet
     card_strengths = tuple(strengths[card] for card in hand)
-    ofakind = Counter(Counter(hand).values())
-    if ofakind == {5: 1}:
+    of_a_kind = Counter(Counter(hand).values())
+    if of_a_kind == {5: 1}:
         return 5, card_strengths
-    elif ofakind == {4: 1, 1: 1}:
+    elif of_a_kind == {4: 1, 1: 1}:
         return 4, card_strengths
-    elif ofakind == {3: 1, 2: 1}:
+    elif of_a_kind == {3: 1, 2: 1}:
         return 3.5, card_strengths
-    if ofakind == {3: 1, 1: 2}:
+    if of_a_kind == {3: 1, 1: 2}:
         return 3, card_strengths
-    if ofakind == {2: 2, 1: 1}:
+    if of_a_kind == {2: 2, 1: 1}:
         return 2, card_strengths
-    if ofakind == {2: 1, 1: 3}:
+    if of_a_kind == {2: 1, 1: 3}:
         return 1, card_strengths
-    if ofakind == {1: 5}:
+    if of_a_kind == {1: 5}:
         return 0, card_strengths
-    raise ValueError(str(ofakind))
+    raise ValueError(str(of_a_kind))
 
 
 def part1(raw: str):
     lines = list(parse(raw))
-    fugg = sorted(lines, key=key)
+    sorted_lines = sorted(lines, key=key)
     s = 0
-    for i, (hand, bet) in enumerate(fugg):
+    for i, (hand, bet) in enumerate(sorted_lines):
         s += (i + 1) * bet
     return s
-
-
-strengths = "A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".split(", ")
-strengths = {ch: -i for i, ch in enumerate(strengths)}
 
 
 def key2(hand_and_bet: (str, int)):
@@ -60,7 +56,7 @@ def key2(hand_and_bet: (str, int)):
         return 5, card_strengths  # Five of a kind
     elif of_a_kind in ({4: 1, 1: 1}, {3: 1, 1: 1}, {2: 1, 1: 1}, {1: 2}):
         return 4, card_strengths  # Four of a kind
-    elif of_a_kind in ({3: 1, 2: 1}, {2: 2}, {3: 1, 1: 1}):
+    elif of_a_kind in ({3: 1, 2: 1}, {2: 2}):
         return 3.5, card_strengths  # Full house
     elif of_a_kind in ({3: 1, 1: 2}, {1: 2, 2: 1}, {1: 3}):
         return 3, card_strengths  # Three of a kind
@@ -75,17 +71,11 @@ def key2(hand_and_bet: (str, int)):
 
 def part2(raw: str):
     lines = list(parse(raw))
-    for line in lines:
-        if not key2(line) >= key(line):
-            print(line, key2(line))
-    fugg = sorted(lines, key=key2)
-    assert len(set(map(key2, lines))) == len(lines)
+    sorted_lines = sorted(lines, key=key2)
     s = 0
-    for i, (hand, bet) in enumerate(fugg):
+    for i, (hand, bet) in enumerate(sorted_lines):
         s += (i + 1) * bet
     return s
-    # not 252122402
-    # not 252474561
 
 
 def main():
