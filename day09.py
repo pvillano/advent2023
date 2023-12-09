1894233af1cbc9c082b98ac4c0f7b6bca65cd90a
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2023-12-08 23:20:32 -0600

    day 9 clean

--- day09.py
+++ day09.py
@@ -1,3 +1,6 @@
+import operator as op
+from itertools import starmap
+
 from utils import benchmark, get_day, test, extract_ints
 
 test1 = """0 3 6 9 12 15
@@ -16,19 +19,19 @@ def parse(raw: str):
     return [list(extract_ints(line)) for line in lines]
 
 
-def zoop(x):
-    r = list(map(lambda a: a[1] - a[0], zip(x, x[1:])))
+def difference_operator(x):
+    r = list(starmap(op.sub, zip(x[1:], x)))
     assert len(r) == len(x) - 1
     return r
 
 
 def part1(raw: str):
-    historys = parse(raw)
+    histories = parse(raw)
     s = 0
-    for history in historys:
+    for history in histories:
         tree = [history]
         while any(tree[-1]):
-            tree.append(zoop(tree[-1]))
+            tree.append(difference_operator(tree[-1]))
         tree = list(reversed(tree))
         for i in range(1, len(tree)):
             tree[i].append(tree[i][-1] + tree[i - 1][-1])
@@ -37,26 +40,15 @@ def part1(raw: str):
 
 
 def part2(raw: str):
-    historys = parse(raw)
-    s = 0
-    for history in historys:
-        tree = [history]
-        while any(tree[-1]):
-            tree.append(zoop(tree[-1]))
-        tree = list(reversed(tree))
-
-        for i in range(1, len(tree)):
-            x = tree[i][0] - tree[i - 1][0]
-            tree[i] = [x] + tree[i]
-        s += tree[-1][0]
-    return s
+    raw2 = "\n".join(" ".join(reversed(line.split())) for line in raw.splitlines())
+    return part1(raw2)
 
 
 def main():
     test(part1, test1, expected1)
+    test(part2, test2, expected2)
     raw = get_day(9, override=True)
     benchmark(part1, raw)
-    test(part2, test2, expected2)
     benchmark(part2, raw)
 
 
