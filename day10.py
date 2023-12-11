43469ab1484a93c663b213d8dc974b9fec048453
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2023-12-11 11:02:24 -0600

    day 10 optimization

--- day10.py
+++ day10.py
@@ -2,7 +2,7 @@ import sys
 from collections import deque
 from math import inf
 
-from utils import benchmark, get_day, test, debug_print_sparse_grid
+from utils import benchmark, get_day, test
 
 test1 = """..F7.
 .FJ|.
@@ -38,151 +38,145 @@ def parse(raw: str):
     return raw.splitlines()
 
 
+def connected(lines, r1, c1, r2, c2):
+    if lines[r1][c1] == '.' or lines[r2][c2] == '.':
+        return False
+    if r1 == r2:
+        if c1 > c2:
+            c1, c2 = c2, c1
+        assert c1 < c2
+        ch1, ch2 = lines[r1][c1], lines[r2][c2]
+        return "e" in adjacency[ch1] and "w" in adjacency[ch2]
+    else:
+        assert c1 == c2
+        if r1 > r2:
+            r1, r2 = r2, r1
+        assert r1 < r2
+        ch1, ch2 = lines[r1][c1], lines[r2][c2]
+        return "s" in adjacency[ch1] and "n" in adjacency[ch2]
+
+
+def find_s(lines):
+    for r, line in enumerate(lines):
+        for c, char in enumerate(line):
+            if char == "S":
+                return r, c
+
+
 def part1(raw: str):
     lines = parse(raw)
 
-    def connected(rr, cc, rrr, ccc):
-        if rr == rrr:
-            if cc > ccc:
-                cc, ccc = ccc, cc
-            assert cc < ccc
-            ch1, ch2 = lines[rr][cc], lines[rrr][ccc]
-            return "e" in adjacency[ch1] and "w" in adjacency[ch2]
-        assert cc == ccc
-        if rr > rrr:
-            rr, rrr = rrr, rr
-        assert rr < rrr
-        ch1, ch2 = lines[rr][cc], lines[rrr][ccc]
-        return "s" in adjacency[ch1] and "n" in adjacency[ch2]
+    origin_r, origin_c = find_s(lines)
 
-    for ra, line in enumerate(lines):
-        for ca, char in enumerate(line):
-            if char == "S":
-                r, c = ra, ca
-                break
+    def find_furthest():
+        distances = [[inf] * len(l) for l in lines]
+        distances[origin_r][origin_c] = 0
+        best = 0
+        todo = deque([(origin_r, origin_c)])
+        while todo:
+            r, c = todo.popleft()
+            assert distances[r][c] <= best
+            assert lines[r][c] != '.'
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                r2, c2 = r + dx, c + dy
+                if r2 not in range(len(lines)) or c2 not in range(len(lines[0])):
+                    continue
+                if distances[r2][c2] != inf:
+                    continue
+                if not connected(lines, r, c, r2, c2):
+                    continue
+                distances[r2][c2] = distances[r][c] + 1
+                todo.append((r2, c2))
+                best = max(best, distances[r2][c2])
+        return best
 
-    distances = [[inf] * len(l) for l in lines]
-    distances[r][c] = 0
-    best = 0
-    todo = deque([(r, c)])
-    while todo:
-        assert distances[r][c] <= best
-        assert lines[r][c] != '.'
-        r, c = todo.popleft()
-        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            r2, c2 = r + dx, c + dy
-            if r2 not in range(len(lines)) or c2 not in range(len(lines[0])):
-                continue
-            if lines[r2][c2] == '.':
-                continue
-            if distances[r2][c2] != inf:
-                continue
-            if not connected(r, c, r2, c2):
-                continue
-            distances[r2][c2] = distances[r][c] + 1
-            todo.append((r2, c2))
-            best = max(best, distances[r2][c2])
-    return best
+    return find_furthest()
 
 
 def part2(raw: str):
     lines = parse(raw)
 
-    def connected(rr, cc, rrr, ccc):
-        if lines[rr][cc] == '.' or lines[rrr][ccc] == '.':
-            return False
-        if rr == rrr:
-            if cc > ccc:
-                cc, ccc = ccc, cc
-            assert cc < ccc
-            ch1, ch2 = lines[rr][cc], lines[rrr][ccc]
-            return "e" in adjacency[ch1] and "w" in adjacency[ch2]
-        assert cc == ccc
-        if rr > rrr:
-            rr, rrr = rrr, rr
-        assert rr < rrr
-        ch1, ch2 = lines[rr][cc], lines[rrr][ccc]
-        return "s" in adjacency[ch1] and "n" in adjacency[ch2]
-
-    for find_s_r, line in enumerate(lines):
-        for find_s_c, char in enumerate(line):
-            if char == "S":
-                origin_r, origin_c = find_s_r, find_s_c
-                break
-    origin = origin_r, origin_c
+    origin_r, origin_c = find_s(lines)
 
     stack = []
     sys.setrecursionlimit(15000)
 
-    def rec(r, c):
+    def recurse(r, c):
         assert lines[r][c] != '.'
-        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            r2, c2 = r + dr, c + dc
+        for delta_r, delta_c in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            r2, c2 = r + delta_r, c + delta_c
             if r2 not in range(len(lines)) or c2 not in range(len(lines[0])):
                 continue
-            if lines[r2][c2] == '.':
-                continue
             if stack and (r2, c2) == stack[-1]:
+                continue
+            if not connected(lines, r, c, r2, c2):
                 continue
             if stack and (r2, c2) == stack[0]:
                 stack.append((r, c))
                 return True
-            if not connected(r, c, r2, c2):
-                continue
             stack.append((r, c))
-            finished = rec(r2, c2)
+            finished = recurse(r2, c2)
             if finished:
                 return True
             stack.pop()
         return False
 
-    rec(*origin)
+    recurse(origin_r, origin_c)
     assert stack
-    debug_print_sparse_grid(set(stack), transpose=True)
-    pass
-    to_flood = deque([(.5, .5)])
-    flooded = set()
-    while to_flood:
-        origin_r, origin_c = to_flood.popleft()
-        if (origin_r, origin_c) in flooded:
-            continue
-        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            r2, c2 = origin_r + dr, origin_c + dc
-            if not (0 <= r2 < len(lines) - .5 and 0 <= c2 < len(lines[0]) - .5):
-                continue
-            if origin_r != r2:
-                r_avg = int((origin_r + r2) / 2)
-                if not connected(r_avg, int(origin_c + .5), r_avg, int(origin_c - .5)):
-                    to_flood.appendleft((r2, c2))
-                elif (r_avg, int(origin_c + .5)) not in stack or (r_avg, int(origin_c - .5)) not in stack:
-                    to_flood.appendleft((r2, c2))
-            else:
-                c_avg = int((origin_c + c2) / 2)
-                if not connected(int(origin_r - .5), c_avg, int(origin_r + .5), c_avg):
-                    to_flood.appendleft((r2, c2))
-                elif (int(origin_r - .5), c_avg) not in stack or (int(origin_r + .5), c_avg) not in stack:
-                    to_flood.appendleft((r2, c2))
+    stack = {rc: i for i, rc in enumerate(stack)}
+    # debug_print_sparse_grid(set(stack), transpose=True)
 
-        flooded.add((origin_r, origin_c))
-    s = 0
-    for r1 in range(len(lines)):
-        for c1 in range(len(lines[0])):
-            wet = True
-            for dr, dc in [(-1, -1), (1, 1), (1, -1), (-1, 1)]:
-                corner_r = r1 + dr / 2
-                corner_c = c1 + dc / 2
-                if not (0 <= corner_r < len(lines) - .5 and 0 <= corner_c < len(lines[0]) - .5):
-                    continue  # outside corners are wet
-                if (corner_r, corner_c) not in flooded:
-                    wet = False
-                    break
+    def flood():
+        seen = set()
+        to_flood = deque([(.5, .5)])
+        while to_flood:
+            src_r, src_c = to_flood.popleft()
+            if (src_r, src_c) in seen:
+                continue
+            for delta_r, delta_c in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                dest_r, dest_c = src_r + delta_r, src_c + delta_c
+                if not (0.5 <= dest_r < len(lines) - .5 and 0.5 <= dest_c < len(lines[0]) - .5):
+                    continue
+                if src_r != dest_r:
+                    r_avg = int((src_r + dest_r) / 2)
+                    if (r_avg, int(src_c + .5)) not in stack or (r_avg, int(src_c - .5)) not in stack:
+                        to_flood.appendleft((dest_r, dest_c))
+                    elif not connected(lines, r_avg, int(src_c + .5), r_avg, int(src_c - .5)):
+                        to_flood.appendleft((dest_r, dest_c))
                 else:
-                    pass
-            if wet:
-                s += 1
-    a_stack = len(stack)
-    a_tot = len(lines) * len(lines[0])
-    return a_tot - a_stack - s
+                    c_avg = int((src_c + dest_c) / 2)
+                    if (int(src_r - .5), c_avg) not in stack or (int(src_r + .5), c_avg) not in stack:
+                        to_flood.appendleft((dest_r, dest_c))
+                    elif not connected(lines, int(src_r - .5), c_avg, int(src_r + .5), c_avg):
+                        to_flood.appendleft((dest_r, dest_c))
+            seen.add((src_r, src_c))
+        return seen
+
+    flooded = flood()
+
+    def count_wet():
+        wet_count = 0
+        for r in range(len(lines)):
+            for c in range(len(lines[0])):
+                wet = True
+                for delta_r, delta_c in [(-1, -1), (1, 1), (1, -1), (-1, 1)]:
+                    corner_r = r + delta_r / 2
+                    corner_c = c + delta_c / 2
+                    if not (0 <= corner_r < len(lines) - .5 and 0 <= corner_c < len(lines[0]) - .5):
+                        continue  # outside corners are wet
+                    if (corner_r, corner_c) not in flooded:
+                        wet = False
+                        break
+                    else:
+                        pass
+                if wet:
+                    wet_count += 1
+        return wet_count
+
+    area_wet = count_wet()
+    area_stack = len(stack)
+    area_total = len(lines) * len(lines[0])
+    return area_total - area_stack - area_wet
 
 
 def main():
