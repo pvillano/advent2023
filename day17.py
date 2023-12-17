a9fd6ef9d3ff69ad085c55ce81e4f7467d00f607
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2023-12-17 00:49:21 -0600

    17.2

--- day17.py
+++ day17.py
@@ -1,7 +1,8 @@
-from cmath import inf
 from collections import deque
 
-from utils import benchmark, get_day, test
+import numpy as np
+
+from utils import benchmark, get_day, test, debug_print, debug_print_grid
 from utils.grids import NEWS_RC
 
 test1 = """2413432311323
@@ -21,7 +22,14 @@ test1 = """2413432311323
 expected1 = 102
 
 test2 = test1
-expected2 = None
+expected2 = 94
+
+test22 = """111111111111
+999999999991
+999999999991
+999999999991
+999999999991"""
+expected22 = 71
 
 
 def parse(raw: str):
@@ -35,18 +43,19 @@ def part1(raw: str):
     grid = parse(raw)
     row_count = len(grid)
     col_count = len(grid[0])
-    seen = {(0, 0, 0, 0, 0): 0}
+    seen = np.full((row_count, col_count, 3, 3, 4), np.iinfo(int).max)
+    seen[0, 0, :, :, :] = 0
     q = deque([(0, 0, 0, 0, 0)])
 
-    worst = inf
     while q:
         r, c, dr, dc, t = q.pop()
-        d = seen[(r, c, dr, dc, t)]
+        d = seen[r, c, dr, dc, t]
+        assert 0 <= d < np.iinfo(int).max
         for next_dr, next_dc in NEWS_RC:
             if next_dr == -dr and next_dc == -dc:
                 continue
             if next_dr == dr and next_dc == dc:
-                if t == 3:  # todo
+                if t == 3:
                     continue
                 t2 = t + 1
             else:
@@ -55,30 +64,72 @@ def part1(raw: str):
             if r2 not in range(row_count) or c2 not in range(col_count):
                 continue
             d2 = d + grid[r2][c2]
-            if d2 >= worst:
-                continue
-            if r2 == row_count - 1 and c2 == col_count - 1:
-                worst = min(worst, d2)
-            best = inf
-            for i in range(t2):
-                best = min(best, seen.get((r2, c2, next_dr, next_dc, t2), inf))
+            # if d2 >= worst:
+            #     continue
+            # if r2 == row_count - 1 and c2 == col_count - 1:
+            #     worst = min(worst, d2)
+            best = seen[r2, c2, next_dr, next_dc, t2]
             if d2 < best:
-                seen[(r2, c2, next_dr, next_dc, t2)] = d2
-                q.append((r2, c2, next_dr, next_dc, t2))
+                seen[r2, c2, next_dr, next_dc, t2] = d2
+                q.appendleft((r2, c2, next_dr, next_dc, t2))
 
-    best_keys = [x for x in seen.keys() if x[0] == row_count - 1 and x[1] == col_count - 1]
-    return min(seen[k] for k in best_keys)
+    return seen[-1, -1, :, :, :].min()
+
+
+# Completed in 369.942 seconds. kek
 
 
 def part2(raw: str):
-    lines = parse(raw)
+    grid = parse(raw)
+    row_count = len(grid)
+    col_count = len(grid[0])
+    seen = np.full((row_count, col_count, 3, 3, 11), np.iinfo(int).max)
+    seen[0, 0, :, :, :] = 0
+    q = deque([(0, 0, 0, 1, 0), (0, 0, 1, 0, 0)])
+
+    worst = np.iinfo(int).max
+    while q:
+        r, c, dr, dc, t = q.pop()
+        d = seen[r, c, dr, dc, t]
+        assert 0 <= d < np.iinfo(int).max
+        for next_dr, next_dc in NEWS_RC:
+            if next_dr == -dr and next_dc == -dc:
+                continue
+            if next_dr == dr and next_dc == dc:
+                if t >= 10:
+                    continue
+                t2 = t + 1
+            else:
+                if t < 4:
+                    continue
+                else:
+                    t2 = 1
+
+            r2, c2 = r + next_dr, c + next_dc
+            if r2 not in range(row_count) or c2 not in range(col_count):
+                continue
+            d2 = d + grid[r2][c2]
+            if d2 >= worst:
+                continue
+            if r2 == row_count - 1 and c2 == col_count - 1 and t2 >= 4:
+                worst = min(worst, d2)
+                print(worst, len(q))
+            best = seen[r2, c2, next_dr, next_dc, t2]
+            if d2 < best:
+                seen[r2, c2, next_dr, next_dc, t2] = d2
+                q.appendleft((r2, c2, next_dr, next_dc, t2))
+    debug_print_grid(seen.min((2, 3, 4)))
+    for i in range(10):
+        debug_print(i, seen[-1, -1, :, :, i].min())
+    return seen[-1, -1, :, :, 4:].min()
 
 
 def main():
     test(part1, test1, expected1)
     raw = get_day(17, override=True)
-    benchmark(part1, raw)
+    # benchmark(part1, raw)
     test(part2, test2, expected2)
+    test(part2, test22, expected22)
     benchmark(part2, raw)
 
 
