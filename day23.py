cdc73761a1bd873ab03956ae4d4e2ef86066e3ab
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2024-01-11 10:21:53 -0600

    23p1

--- day23.py
+++ day23.py
@@ -1,9 +1,9 @@
+import sys
 from collections import defaultdict, Counter
 
-from utils import benchmark, get_day, test, debug_print_grid, debug_print_sparse_grid
+from utils import benchmark, get_day, test, debug_print_sparse_grid, debug_print
 from utils.graphs import topological_sort, reverse_edges
 from utils.grids import NEWS_RC
-
 
 Counter
 
@@ -16,12 +16,13 @@ def parse(raw: str):
 
 
 def part1(raw: str):
-
     lines = parse(raw)
     n_rows = len(lines)
     n_cols = len(lines[0])
-    start = (0,1)
-    end = (n_rows-1, n_cols-2)
+    start = (0, 1)
+    assert lines[start[0]][start[1]] == "."
+    end = (n_rows - 1, n_cols - 2)
+    assert lines[end[0]][end[1]] == "."
     critical = set()
     as_graph = defaultdict(list)
     for row in range(n_rows):
@@ -31,31 +32,38 @@ def part1(raw: str):
             if lines[row][col] in "^<>v":
                 for dr, dc in NEWS_RC:
                     if (lines[row][col], (dr, dc)) in zip("^><v", NEWS_RC):
-                        r2, c2 = row+dr, col+dc
-                        as_graph[(row,col)] = [(r2, c2)]
+                        r2, c2 = row + dr, col + dc
+                        as_graph[(row, col)] = [(r2, c2)]
                         break
                 continue
             for dr, dc in NEWS_RC:
-                r2, c2 = row+dr, col+dc
+                r2, c2 = row + dr, col + dc
                 if r2 not in range(n_rows) or c2 not in range(n_cols):
                     continue
                 ch = lines[r2][c2]
                 if ch == "#":
                     continue
-                if ch == ".":
-                    as_graph[(row, col)].append((r2, c2))
-                    continue
-                if (ch, (dr, dc)) in zip("v<>^", NEWS_RC):
-                    continue
                 as_graph[(row, col)].append((r2, c2))
-    preds = reverse_edges(as_graph)
-    longest_ending_at = defaultdict(int)
-    for v in topological_sort(as_graph):
-        longest_ending_at[v] = max(longest_ending_at[v2] for v2 in preds[v]) + 1
-    debug_print_sparse_grid(longest_ending_at, transpose=True)
-    return longest_ending_at[end]
+    stack = []
+    sys.setrecursionlimit(15000)
+    best = 0
 
-
+    def recurse(node):
+        stack.append(node)
+        for next_node in as_graph[node]:
+            if next_node != stack[-1] and next_node not in stack:
+                if next_node == end:
+                    assert len(stack) == len(set(stack))
+                    # debug_print_sparse_grid(stack, transpose=True)
+                    yield len(stack)
+                else:
+                    yield from recurse(next_node)
+        stack.pop()
+    for path in recurse(start):
+        debug_print(path, stack)
+        best = max(best, path)
+    # assert not best == XXXX
+    return best
 
 
 def part2(raw: str):
