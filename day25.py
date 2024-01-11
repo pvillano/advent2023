df4fbe5c55ccf784a3a7c86916b44fbdd2337786
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2024-01-11 18:36:54 -0500

    25p1 I ain't implementing that lol

--- day25.py
+++ day25.py
@@ -1,8 +1,6 @@
-import random
-from collections import defaultdict
+import networkx as nx
 
 from utils import benchmark, get_day, test
-from utils.graphs import densify_keys
 
 
 def parse(raw: str):
@@ -14,64 +12,12 @@ def parse(raw: str):
     return ret
 
 
-def find_set(u, forest):
-    while forest[u] != u:
-        u, forest[u] = forest[u], forest[forest[u]]
-    return u
-
-
-def union_set(u, v, forest, sizes):
-    u = find_set(u, forest)
-    v = find_set(v, forest)
-    if u == v:
-        return
-    if sizes[u] < sizes[v]:
-        u, v = v, u
-    assert sizes[u] >= sizes[v]
-    forest[v] = u  # u is new root
-    sizes[u] += sizes[v]
-
-
-def run_iter(new_adj_list, weight_sorted_edges):
-    forest = list(range(len(new_adj_list)))
-    sizes = [1] * len(new_adj_list)
-    for i, (u, v) in enumerate(weight_sorted_edges):
-        u = find_set(u, forest)
-        v = find_set(v, forest)
-        if u != v:
-            if sizes[u] + sizes[v] == len(sizes):
-                return len(weight_sorted_edges) - i, sizes[u] * sizes[v]
-            union_set(u, v, forest, sizes)
-
-
 def part1(raw: str):
     lines = parse(raw)
-    bidir_str_graph = {key: set(value) for key, value in lines.items()}
-    bidir_str_graph = defaultdict(set, bidir_str_graph)
-    for key, values in lines.items():
-        for value in values:
-            bidir_str_graph[value].add(key)
-    new_adj_list, _, _ = densify_keys(bidir_str_graph)
-    del bidir_str_graph, lines
-    weight_sorted_edges = []
-    for key, values in enumerate(new_adj_list):
-        for value in values:
-            if key < value:
-                weight_sorted_edges.append((key, value))
-    best = (len(weight_sorted_edges), 0)
-    while True:
-        random.shuffle(weight_sorted_edges)
-        candidate = run_iter(new_adj_list, weight_sorted_edges)
-        if candidate < best:
-            best = candidate
-            print(best)
-            count, product = candidate
-            if count == 3:
-                return product
-
-
-def part2(raw: str):
-    lines = parse(raw)
+    g = nx.Graph(lines)
+    cut_value, partition = nx.stoer_wagner(g)
+    assert cut_value == 3
+    return len(partition[0]) * len(partition[1])
 
 
 test1 = """jqt: rhn xhk nvd
@@ -90,16 +36,11 @@ frs: qnr lhk lsr"""
 
 expected1 = 54
 
-test2 = test1
-expected2 = None
-
 
 def main():
     test(part1, test1, expected1)
     raw = get_day(25, override=True)
     benchmark(part1, raw)
-    test(part2, test2, expected2)
-    benchmark(part2, raw)
 
 
 if __name__ == "__main__":
